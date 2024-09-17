4ff3b497cabcb2a7a8df18c14ee7ef43d61a5770
George FunBook <gkurelic@gmail.com>
2024-09-17 11:59:44 -0500

    fix warnings

--- samples/demo/source/PlayState.hx
+++ samples/demo/source/PlayState.hx
@@ -8,7 +8,7 @@ import flixel.tweens.FlxTween;
 import flixel.tweens.FlxTween.FlxTweenType;
 import flixel.tweens.FlxEase;
 import flixel.system.FlxAssets;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 
 import textbox.Textbox;
 import textbox.Settings;
@@ -29,7 +29,6 @@ class DemoTextCursor extends FlxSprite
 			characterCallbackInternal(character);
 		};
 	}
-
 
 	public function attachToTextbox(textbox:Textbox)
 	{
